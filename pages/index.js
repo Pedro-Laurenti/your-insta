7b03a32da349e05da7e73b1cfe55051b31a9da79
Pedro Laurenti <pedrolaurenti04@gmail.com>
2024-02-29 22:57:42 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import {useState} from 'react';
 function Home(){
     return(
         <div>
-            <h1> AOOOOO POTEEEENCIA </h1>
+            <h1> AOOOOO POTEEEENCIa </h1>
         </div>
     )
 }
